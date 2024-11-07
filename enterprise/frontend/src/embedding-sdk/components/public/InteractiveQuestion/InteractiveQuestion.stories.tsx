8f74a9dc0b4b9684a5db0b4370a1028d8eeb0ad0
Mahatthana (Kelvin) Nomsawadi <me@bboykelvin.dev>
2024-11-07 22:01:23 +0700

    [debug] add storybook helper

--- enterprise/frontend/src/embedding-sdk/components/public/InteractiveQuestion/InteractiveQuestion.stories.tsx
+++ enterprise/frontend/src/embedding-sdk/components/public/InteractiveQuestion/InteractiveQuestion.stories.tsx
@@ -5,7 +5,13 @@ import { CommonSdkStoryWrapper } from "embedding-sdk/test/CommonSdkStoryWrapper"
 
 import { InteractiveQuestion } from "./InteractiveQuestion";
 
-const QUESTION_ID = (window as any).QUESTION_ID || 12;
+const NUMBER_ID = 12;
+const ENTITY_ID = "evd6f3MoDUtTNsoXHwu0T";
+const ONE_TOO_MANY_ENTITY_ID = ENTITY_ID + "1";
+const WRONG_ENTITY_ID = ENTITY_ID.slice(0, -1) + "1";
+const WRONG_NUMBER_ID = 99999999;
+
+const QUESTION_ID = ENTITY_ID;
 
 type InteractiveQuestionComponentProps = ComponentProps<
   typeof InteractiveQuestion
@@ -18,6 +24,27 @@ export default {
     layout: "fullscreen",
   },
   decorators: [CommonSdkStoryWrapper],
+  argTypes: {
+    questionId: {
+      options: [
+        ENTITY_ID,
+        ONE_TOO_MANY_ENTITY_ID,
+        WRONG_ENTITY_ID,
+        NUMBER_ID,
+        WRONG_NUMBER_ID,
+      ],
+      control: {
+        type: "select",
+        labels: {
+          [ENTITY_ID]: "Entity ID",
+          [ONE_TOO_MANY_ENTITY_ID]: "One Too Many Entity ID",
+          [WRONG_ENTITY_ID]: "Wrong Entity ID",
+          [NUMBER_ID]: "Number ID",
+          [WRONG_NUMBER_ID]: "Wrong Number ID",
+        },
+      },
+    },
+  },
 };
 
 const Template: StoryFn<InteractiveQuestionComponentProps> = args => {
